7bdc246e60ac0e69571b6a7b5462c6d37738e2a1
Nathanstuff <n.odonnell@hotmail.com>
2020-11-02 15:13:45 +1100

    new task controller and view created

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,19 @@
 class TasksController < ApplicationController
+    
+    def new
+        @task = Task.new
+    end
+    
+    def create
+        @task = Task.new(task_params)
+        @task.save
+
+        redirect_to @task
+    end
+
+    private
+    def task_params
+        params.require(:task).permit(:title, :description, :category, :priority)
+    end
+
 end
