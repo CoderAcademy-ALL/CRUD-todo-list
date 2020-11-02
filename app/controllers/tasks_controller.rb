2b83fc395a24014a506ef07a6e17e8b94d18416b
Nathanstuff <n.odonnell@hotmail.com>
2020-11-02 17:57:38 +1100

    edit task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,16 +23,31 @@ class TasksController < ApplicationController
     end
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+   
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      redner 'edit'
+    end
+  end
+
   private
   def task_params
       params.require(:task).permit(:title, :description, :category, :priority, :complete)
   end
 
-    def destroy
-        @task = Task.find(params[:id])
-        @task.destroy
-       
-        redirect_to tasks_path
-      end
+    
 
 end
