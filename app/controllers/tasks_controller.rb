aeeeb2cc28d3c399f6147f8b7b30e5cc2376aeed
pin0S <pete.g.lynch@gmail.com>
2020-11-02 15:23:51 +1100

    delete added to show and link to article added to index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,4 +11,11 @@ class TasksController < ApplicationController
         @task = Task.find(params[:id])
     end
 
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+       
+        redirect_to tasks_path
+      end
+
 end
