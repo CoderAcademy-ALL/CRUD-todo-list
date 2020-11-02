51b5dc42392c69e6017af15edf0995c6373ca16b
Nathanstuff <n.odonnell@hotmail.com>
2020-11-02 16:24:54 +1100

    flash and form for task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,14 +16,16 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    @task.save
-
-    redirect_to @task
+    if @task.save
+      redirect_to @task
+    else
+      render 'new'
+    end
   end
 
   private
   def task_params
-      params.require(:task).permit(:title, :description, :category, :priority)
+      params.require(:task).permit(:title, :description, :category, :priority, :complete)
   end
 
 end
